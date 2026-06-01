9c29b9068c888a70ea80c8ed68d7018333f249ec
Paulette246 <2425400669@live.gctu.edu.gh>
2026-06-01 16:57:48 +0000

    Add files via upload

---
+++ initials.py.py
@@ -0,0 +1,39 @@
+#File: initials.py
+#Description: A program that prints the stylized initials PEA.
+#Assignment Number: 1
+#
+#Name: Paulette EFFUA Arthur
+#Student ID: 2425400669
+#Email: 2425400669@live.gctu.edu.gh
+#Grader: Mr. Augustus Buckman
+#
+#On my honor, Paulette EFFUA Arthur, this programming assignment is my own work
+#and I have not provided this code to any other student.
+
+
+def main():
+    #Prints a small and large styled initials PEA with strict grid formatting.
+#Your print statements go here
+
+
+    print()
+
+    print("...PEA")
+
+    print()
+
+    print("...PPPPPPPPPPPP........EEEEEEEEEEEE................AA........")
+    print("...PP.............PP...EE.........................AAAA........")
+    print("...PP.............PP...EE........................AA..AA.......")
+    print("...PPPPPPPPPPPP........EEEEEEEE..................AA..AA.......")
+    print("...PP..................EEEEEEEE.................AAAAAAAA......")
+    print("...PP..................EE.......................AAAAAAAA......")
+    print("...PP..................EE......................AA......AA.....")
+    print("...PP..................EE.....................AA........AA....")
+    print("...PP........**........EE....**..............AA..........AA....**")
+    print("...PP.........**.......EEEEEEEEEEEE....**...AA............AA....**")
+
+    print()
+
+
+main()
